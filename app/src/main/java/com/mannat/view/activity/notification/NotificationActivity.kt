18a0b23c9bce49c4836eb6f5803d16765fe49a39
vkveenkumar <vkveenkumar@gmail.com>
2022-08-08 12:18:46 +0530

    08Aug2022

--- app/src/main/java/com/mannat/view/activity/notification/NotificationActivity.kt
+++ app/src/main/java/com/mannat/view/activity/notification/NotificationActivity.kt
@@ -2,6 +2,7 @@ package com.mannat.view.activity.notification
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mannat.base.BaseActivity
 import com.mannat.databinding.ActivityNotificationBinding
@@ -15,6 +16,9 @@ class NotificationActivity : BaseActivity<ActivityNotificationBinding>() {
 
     override fun onViewBindingCreated(savedInstanceState: Bundle?) {
         super.onViewBindingCreated(savedInstanceState)
+
+        binding.include2.tHeading.text = "Notifications"
+        binding.include2.tNotification.visibility = View.GONE
 
         binding.notificationRecyclerView.layoutManager =
             LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
--- app/src/main/java/com/mannat/view/fragment/bottomnav/ProfileFragment.kt
+++ app/src/main/java/com/mannat/view/fragment/bottomnav/ProfileFragment.kt
@@ -5,13 +5,30 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.mannat.base.BaseFragment
 import com.mannat.databinding.FragmentProfileBinding
+import com.mannat.view.adapter.ViewPagerAdapter
+import com.mannat.view.fragment.hometabs.AboutGodsFragment
+import com.mannat.view.fragment.hometabs.NewPostFragment
+import com.mannat.view.fragment.hometabs.TrendingFragment
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(){
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentProfileBinding
         get() = FragmentProfileBinding::inflate
+    private lateinit var adapter: ViewPagerAdapter
 
     override fun onViewBindingCreated(savedInstanceState: Bundle?) {
         super.onViewBindingCreated(savedInstanceState)
 
+        setupViewPager()
+
+    }
+
+    private fun setupViewPager() {
+        adapter = ViewPagerAdapter(activity!!.supportFragmentManager)
+        adapter.addFragment(NewPostFragment(), "About")
+        adapter.addFragment(TrendingFragment(), "Upload")
+        adapter.addFragment(AboutGodsFragment(), "Profile")
+        binding.vpOrder.adapter = adapter
+        binding.tabLayout2.setupWithViewPager(binding.vpOrder)
+        adapter.notifyDataSetChanged()
     }
 }
